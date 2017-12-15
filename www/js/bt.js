1ce783e5df6a8bfbba4363841130a9a6732bbcd1
romanlutai <33683663+romanlutai@users.noreply.github.com>
2017-12-15 14:26:53 +0200

    Major update
    Changed design;
    Added color circle;
    [debugging] added menu

--- www/js/bt.js
+++ www/js/bt.js
@@ -1,12 +1,34 @@
-// const device = require('device');
-// const bluetoothSerial = require('bluetoothSerial');
-
-//var btList = null;
-
+var btListHTML = document.getElementById('bt_list');
+var greenStatus = document.getElementById('green');
 var bt = {
-  scan: function() {
-    var btListHTML = document.getElementById('bt_list');
+  check: function() {
+    semiLog.log('start');
+    bluetoothSerial.isEnabled(
+//      var btListHTML = document.getElementById('bt_list');
+      function() {
+          console.log("Bluetooth is enabled");
+          semiLog.log("enabled");
+      },
+      function() {
+          console.log("Bluetooth is *not* enabled");
+          semiLog.log("NOT enabled");
+      }
+    );
+    this.list();
+  },
+  list: function() {
+    btListHTML.classList.toggle("show");
     btListHTML.innerHTML = "";
+    bluetoothSerial.list(function(devices) {
+        devices.forEach(function(device) {
+          console.log(device.id);
+          btListHTML.insertAdjacentHTML('afterbegin',`<li onclick="bt.pair('${device.name}','${device.id}')">${device.name}</li>`);
+        });
+      }, function(failure){console.log(failure);}
+    );
+  },
+  scan: function() {
+  btListHTML.innerHTML = "";
     bluetoothSerial.list(function(devices) {
       if ( device.platform == 'Android' ) {
         bluetoothSerial.enable(
@@ -20,17 +42,37 @@ var bt = {
         bluetoothSerial.discoverUnpaired(function(devices) {
             devices.forEach(function(device) {
                 console.log(device.id);
-                btListHTML.insertAdjacentHTML('beforeend',`<li onclick="bt.pair('${device.id}')">${device.name}</li>`);
+                btListHTML.insertAdjacentHTML('beforeend',`<li onclick="bt.pair('${device.name}','${device.id}')">${device.name}</li>`);
             })
         }, function(failure){console.log(failure);} );
       }
       devices.forEach(function(device) {
         console.log(device.id);
-        btListHTML.insertAdjacentHTML('afterbegin',`<li onclick="bt.pair('${device.id}')">${device.name}</li>`);
+        btListHTML.insertAdjacentHTML('afterbegin',`<li onclick="bt.pair('${device.name}','${device.id}')">${device.name}</li>`);
       })
     }, function(failure){console.log(failure);} );
   },
-  pair: function(deviceId) {
-    console.log(`User chose device Id ${deviceId}`);
+  pair: function(deviceName,deviceId) {
+    semiLog.clc();
+    semiLog.log(`You chose device ${deviceName} Id ${deviceId}`);
+    greenStatus.innerHTML = deviceName;
   }
 }
+
+var semiLog = {
+  log: function(message) {
+    logWindow = document.getElementById('semiLog');
+    console.log(message);
+    logWindow.insertAdjacentHTML('beforeend','<div class="logline">'+message+'</div>');
+  },
+  clc: function() {
+    logWindow = document.getElementById('semiLog');
+    logWindow.innerHTML = '';
+  }
+}
+
+function test() {
+  var t = new Date();
+  semiLog.log( t.getTime() );
+}
+
