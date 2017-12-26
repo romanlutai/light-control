a995c018ca6deb804211f1f2dbe30a131c6e004f
romanlutai <33683663+romanlutai@users.noreply.github.com>
2017-12-26 15:46:40 +0200

    btListHTML is object now

--- www/js/bt.js
+++ www/js/bt.js
@@ -1,10 +1,49 @@
-var btListHTML = document.getElementById('bt_list');
+function ListHTML (id) {
+  this.element = document.getElementById(id);
+  this.list = {"50:65:83:9D:05:B3":"MyLamp"};
+
+  this.addBT = (devices) => {
+    var self = this;
+    if (devices instanceof Array) {
+      devices.forEach(function(device){
+        console.log(device.id);
+        self.list[String(device.id)] = device.name;
+      });
+    } else {
+      self.list[String(device.id)] = devices.name;
+    }
+  };
+
+  this.update = () => {
+    var self = this;
+    this.element.innerHTML = "";
+    for (id in this.list){
+      console.log(id);
+      var name = this.list[id];
+      self.element.insertAdjacentHTML('beforeend',`<li onclick="bt.pair('${name}','${id}')">${name}</li>`);
+    };
+  };
+
+  this.hide = () => {
+    if (!this.element.classList.contains('hide')) {
+      this.element.classList.add('hide');
+    }
+  };
+
+  this.show = () => {
+    if (this.element.classList.contains('hide')) {
+      this.element.classList.remove('hide');
+    }
+  };
+}
+
+var btListHTML = new ListHTML('bt_list');
+
 var greenStatus = document.getElementById('green');
+
 var bt = {
   check: function() {
-    if (btListHTML.classList.contains('hide')) {
-      btListHTML.classList.remove('hide');
-    }
+    btListHTML.show();
     semiLog.log('start');
     var self = this;
     bluetoothSerial.isEnabled(
@@ -37,33 +76,35 @@ var bt = {
   },
   list: function() {
     var self = this;
-    btListHTML.innerHTML = "";
     bluetoothSerial.list(function(devices) {
-        devices.forEach(function(device) {
-          console.log(device.id);
-          btListHTML.insertAdjacentHTML('beforeend',`<li onclick="bt.pair('${device.name}','${device.id}')">${device.name}</li>`);
-        });
+        btListHTML.addBT(devices);
+        btListHTML.update();
         if(device.platform == 'Android') self.scan();
       }, function(failure){console.log(failure);}
     );
   },
   scan: function() { // For Android Only
     semiLog.log('Android scanning for unpaired devices');
-    btListHTML.insertAdjacentHTML('beforeend',`<li>scanning...</li>`);
     bluetoothSerial.setDeviceDiscoveredListener(function (device) {
       console.log(device.id);
-      btListHTML.insertAdjacentHTML('beforeend',`<li onclick="bt.pair('${device.name}','${device.id}')">${device.name}</li>`);
+      btListHTML.addBT();
+      btListHTML.update();
     }, function(failure){semiLog.log(failure);} );
     bluetoothSerial.discoverUnpaired( function(devices){
       bluetoothSerial.clearDeviceDiscoveredListener();
     }, function(failure){semiLog.log(failure);} );
   },
   pair: function(deviceName,deviceId) {
-    bluetoothSerial.clearDeviceDiscoveredListener();
-    btListHTML.classList.toggle("hide");
-    semiLog.clc();
-    semiLog.log(`You chose device ${deviceName} Id ${deviceId}`);
-    greenStatus.innerHTML = deviceName;
+    if(device.platform == 'Android') bluetoothSerial.clearDeviceDiscoveredListener();
+    semiLog.log("Connecting to device ${deviceName}");
+    bluetoothSerial.connect(deviceId, function(success){
+      btListHTML.hide();
+      semiLog.clc();
+      semiLog.log(`Successfully connected to the device ${deviceName} Id ${deviceId}`);
+      greenStatus.innerHTML = deviceName;
+    }, function (failure) {
+      semiLog.log("Connection failed: ${failure}");
+    });
   }
 }
 
